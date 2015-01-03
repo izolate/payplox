61b9176bab809709ce96a89fe859640c35e9bc57
yosh <yoshtalwar@gmail.com>
2015-01-03 18:53:06 +0000

    webpack out; browserify in

--- gulp/config.js
+++ gulp/config.js
@@ -1,7 +1,7 @@
 var Config = function () {
   this.assets = 'app/assets';
   this.dist = this.assets + '/dist';
-  this.indexjs = this.assets + '/js/index.js';
+  this.mainjs = this.assets + '/js/index.js';
 };
 
 module.exports = new Config();
