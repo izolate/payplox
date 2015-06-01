a6e8d5d43d1c625fd9c94c6940ae124ada9108c8
yosh <yoshtalwar@gmail.com>
2015-06-01 01:41:14 +0100

    js coding style

--- app/index.js
+++ app/index.js
@@ -1,63 +1,63 @@
-var express = require('express');
-var config = require('./config');
-var app = express();
-var db = require('./db.js').connect();
-var passport = require('passport');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var flash = require('connect-flash');
+const express = require('express')
+const config = require('./config')
+const app = express()
+const db = require('./db.js').connect()
+const passport = require('passport')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const flash = require('connect-flash')
 
-// config ================================================
+// config
 app.use(function (req, res, next) {
-  res.locals.req = req;
-  next();
-});
-app.use(cookieParser());
+  res.locals.req = req
+  next()
+})
+app.use(cookieParser())
 app.use(session({
   secret: 'they were like northern stars',
   resave: true,
   saveUninitialized: true
-}));
-app.use(bodyParser.urlencoded({ extended: true }));
+}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-require('./controllers/csrf')(app);
+require('./controllers/csrf')(app)
 
 // passport
-require('./controllers/passport')(passport);
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
+require('./controllers/passport')(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
 
 // views
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('views', `${__dirname}/views`)
+app.set('view engine', 'jade')
 
 // static files
-app.use('/static', express.static(__dirname + '/static'));
+app.use('/static', express.static(`${__dirname}/static`))
 
-// routes ================================================
-require('./controllers/route')(app);
+// routes
+require('./controllers/route')(app)
 
-[
-  ['/configuration', './routes/configuration'],
-  ['/settings', './routes/settings']
-].forEach(function(route) {
-  app.use(route[0], require(route[1]));
-});
+;[
+  { route: '/configuration', path: './routes/configuration' },
+  { route: '/settings', path: './routes/settings' }
+].forEach(function(module) {
+  app.use(module.route, require(module.path))
+})
 
-[
+;[
   './routes/main',
   './routes/user',
   './routes/clients'
 ].forEach(function(routePath) {
-  require(routePath)(app, passport);
-});
+  require(routePath)(app, passport)
+})
 
-// launch ================================================
+// let's do this
 app.listen(config.express.port, config.express.ip, function (error) {
   if (error)
-    console.error(error);
+    console.error(error)
   else
-    console.log('Payplox listening on localhost: ' + config.express.port);
-});
+    console.log(`Payplox listening on localhost:${config.express.port}`)
+})
