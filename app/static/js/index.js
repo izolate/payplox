a714d9683d5d21faa9bde079b930f221a9237ada
yosh <yoshtalwar@gmail.com>
2015-09-03 23:21:16 +0100

    make settings form work

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,5 +1,6 @@
 import events from 'events'
 import controllers from './ctrl/controllers'
+import { Delegate } from 'dom-delegate'
 
 class App extends events.EventEmitter {
   constructor () {
@@ -8,6 +9,12 @@ class App extends events.EventEmitter {
 }
 
 let app = window.app = new App()
+
+// Event delegation
+window.addEventListener('load', e => {
+  app.delegate = new Delegate(document.body)
+  app.emit('window:load')
+})
 
 // Run controllers
 controllers.forEach(ctrl => ctrl(app))
