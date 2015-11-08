927bc23652319cf5b747bd0f3e17ea93dd32d70a
yosh <yoshtalwar@gmail.com>
2015-11-08 22:37:03 +0000

    Styled login/signup form

--- app/index.js
+++ app/index.js
@@ -12,6 +12,7 @@ app.set('port', process.env.NODE_PORT || 3000)
 app.set('views', `${__dirname}/views`)
 app.set('view engine', 'jade')
 app.use('/static', express.static(`${__dirname}/static`))
+app.use('/img', express.static(`${__dirname}/static/src/img`))
 app.locals.pretty = (process.env.MINIFY === 'false')
 
 // Database
